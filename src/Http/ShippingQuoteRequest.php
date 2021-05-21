54a87884cede15b4ac21566cde4830260da6d2d0
Stoyan Stoimenov <66875479+CyberSecurityBG@users.noreply.github.com>
2021-05-21 10:36:51 +0300

    Add files via upload

--- src/Http/ShippingQuoteRequest.php
+++ src/Http/ShippingQuoteRequest.php
@@ -8,6 +8,10 @@ class ShippingQuoteRequest extends AbstractRequest
 
     public function getData()
     {
+        $cash_on_delivery = 0;
+        if($this->getCashOnDeliveryAmount() > 0){
+            $cash_on_delivery = $this->getCashOnDeliveryAmount();
+        }
         return [
            'FromLocalityId' => $this->getSenderAddress()->getCity()->getId(),
            'ToLocalityId' => $this->getReceiverAddress()->getCity()->getId(),
@@ -15,7 +19,7 @@ class ShippingQuoteRequest extends AbstractRequest
            'Envelopes' => 0,
            'TotalWeight' => (int)$this->getWeight(),
            'DeclaredValue' => ($this->getOtherParameters('declared') == 1) ? $this->getDeclaredAmount() : 0,
-           'CashRepayment' => ($this->getOtherParameters('cd') == 1) ? $this->getCashOnDeliveryAmount() : 0,
+           'CashRepayment' => ($this->getOtherParameters('cd') == 1) ? $cash_on_delivery : 0,
            'BankRepayment' => 0,
            'PaymentInstrumentId' => 0,
            'PaymentInstrumentValue' => 0,
